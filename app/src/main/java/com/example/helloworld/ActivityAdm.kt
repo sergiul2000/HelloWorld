6cfdf58b7d023205be916cf3f123a211a06555e3
Lupsa Sergiu <sergiul2000@yahoo.com>
2022-05-30 20:31:58 +0300

    commit 1

--- app/src/main/java/com/example/helloworld/ActivityAdm.kt
+++ app/src/main/java/com/example/helloworld/ActivityAdm.kt
@@ -5,18 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class activityAdm : AppCompatActivity() {
+class ActivityAdm : AppCompatActivity() {
+
+    /**
+     * this function will open back the MainActivity,and will finish the current
+     * activity
+     */
+    private fun back(){
+        val intent2 = Intent (this,MainActivity::class.java)
+        finish()
+        startActivity(intent2)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adm)
 
-
         val a: Button = findViewById(R.id.backAdm)
 
         a.setOnClickListener{
-
-            val intent2 = Intent (this,MainActivity::class.java)
-            startActivity(intent2)
+            back()
         }
     }
 }
--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -5,37 +5,55 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Switch
-import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
 
+    private var switchState:Boolean=true
+
+    /**
+     * @param firstOpen is a var to see if the switch was touched before or not
+     * @param switch is a val which contain the switch
+     * switchState is a class propriety to see which activity we want to go to
+     * next
+     *
+     * if switchState==true then will open the client activity,
+     * else will open the admin activity
+     *
+     *
+     */
+
+    private fun adminOrClient(firstOpen:Boolean,switch:Switch){
+        switchState= switch.isChecked()
+        val intent:Intent
+        if (firstOpen){
+            if(switchState) {
+                intent = Intent (this,ActivityClient::class.java)
+            }else {
+                intent = Intent (this,ActivityAdm::class.java)
+            }
+            finish()
+            startActivity(intent)
+        }
+    }
+
+    /**
+     * this is the "main" of the activity
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val checkRadioVal:Int;
 
         val btnFirst: Button = findViewById(R.id.loginAdmin)
-        //val btnSecond: Button = findViewById(R.id.loginClient)
+        var firstOpen=true
         val switch:Switch = findViewById(R.id.adminClient)
 
-
-
-
-
-            btnFirst.setOnClickListener{
-                val switchState:Boolean = switch.isChecked();
-                val intent:Intent;
-                if(switchState) {
-                     intent = Intent (this,activityClient::class.java)
-                }else {
-                     intent = Intent (this,activityAdm::class.java)
-
-                }
-                startActivity(intent)
-            }
-                //switchState = switch.isChecked();
-
-
+        /**
+         * this is the action Listener of the button to make it open next
+         * an activity or another
+         */
+        btnFirst.setOnClickListener{
+            adminOrClient(firstOpen,switch)
+            firstOpen=false
         }
-
+    }
 }
